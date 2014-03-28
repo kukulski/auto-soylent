1db9613a20309d7cb281812543e1c82e52bd5e39
Timothy Kukulski <kukulski@adobe.com>
2014-03-27 23:45:33 -0700

    using two ingredient lists
    solver seems to choke when given opportunity to use wierd/bad
    ingredients

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 
 // This can be replaced with any of the recipes on http://diy.soylent.me
 
-var recipeUrls = ["http://diy.soylent.me/recipes/people-chow-301-tortilla-perfection",
-"http://diy.soylent.me/recipes/copy-of-teds-1000-cal-paleo-chow-for-fat-loss-muscle-gain-4"];
-//var nutrientProfile = "51e4e6ca7789bc0200000007"
-var nutrientProfile = "5333c702ec0bea02006ca8cb"
+var recipeUrls = ["http://diy.soylent.me/recipes/people-chow-301-tortilla-perfection"
+    ,"http://diy.soylent.me/recipes/alternate-fats"
+// ,"http://diy.soylent.me/recipes/copy-of-teds-1000-cal-paleo-chow-for-fat-loss-muscle-gain-4"
+
+];
+var nutrientProfile = "51e4e6ca7789bc0200000007"
+//var nutrientProfile = "5333c702ec0bea02006ca8cb"
 // Calorie goal
 var calories = 2200;
 
 // Ratio of carbs / protein / fat. Should add to 100
 var macros = {
-    carbs: 30,
-    protein: 35,
+    carbs: 35,
+    protein: 30,
     fat: 35
 };
 
